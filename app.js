ebd5d502a2fc898d154cfdc53668a36c2b1b669f
jinhyeok99 <143480840+yngjnhykk@users.noreply.github.com>
2024-02-21 13:26:22 +0900

    passport 로그인 구현

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
 
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 
 const db = require('./models');
+const passportConfig = require('./passport');
+
+dotenv.config();
+
 const app = express();
 
 // 서버 연결하면서 db, 시퀄라이즈 연결
@@ -15,6 +23,8 @@ db.sequelize
   })
   .catch(console.error);
 
+passportConfig();
+
 // [CORS error] 모든 브라우저에서 api 사용 허용
 app.use(
   cors({
@@ -24,6 +34,16 @@ app.use(
 // url 로 보낸 데이터를 json 형식으로, form 형식으로 온 데이터를 처리
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('hello express');
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const { User } = require('../models');
 
 const router = express.Router();
 
+// POST /user/login
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.err(err);
+      next(err);
+    }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      return res.json(user);
+    });
+  })(req, res, next);
+});
+
+// POST /user/logout
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.send('로그아웃 성공');
+});
+
+// POST /user
 router.post('/', async (req, res) => {
   try {
     const exUser = await User.findOne({
